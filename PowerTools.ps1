a4e0975cd25f82740aa0ca86cfa3a4f530a714cf
Fr3db3rt <fr3db3rt@gmail.com>
2019-04-03 17:34:33 +0200

    Update PowerTools.ps1

--- PowerTools.ps1
+++ PowerTools.ps1
@@ -1,3 +1,4 @@
+cls
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
@@ -13,14 +14,60 @@ Add-Type -AssemblyName System.Drawing
 	#$mainForm.Topmost = $true
 
 #tabControl
-  $tabControl = New-object Windows.Forms.TabControl
-  $tabControl.Alignment = "Top" #Bottom, Left, Right, Top
-  $tabControl.Appearance = "FlatButtons" #Buttons, FlatButtons, Normal
-  $tabControl.Padding = "10,5"
-  $tabControl.SizeMode = "Normal" #FillToRight, Fixed, Normal
-  $tabControl.Location = "75,85"
-  $tabControl.Size = "800,400"
-  $tabControl.ShowToolTips = "true"
+	$tabControl = New-object Windows.Forms.TabControl
+	$tabControl.Alignment = "Top" #Bottom, Left, Right, Top
+	$tabControl.Appearance = "FlatButtons" #Buttons, FlatButtons, Normal
+	$tabControl.Padding = "10,5"
+	$tabControl.SizeMode = "Normal" #FillToRight, Fixed, Normal
+	$tabControl.Location = "75,85"
+	$tabControl.Size = "800,400"
+	$tabControl.ShowToolTips = "true"
 $mainForm.Controls.Add($tabControl)
+
+#tabPages
+	$ti = 0
+	$tabsCol = @("Gainsboro","Khaki","Bisque")
+	$temp = @()
+	
+	("System","Veeam","WSUS").
+	ForEach{$_
+	write-host "Runde: tabPages ... " $ti
+	write-host "-$_-: " $_
+	$temp += "$_"
+	write-host "-$temp- " $temp[$ti]
+	$_ = New-Object Windows.Forms.TabPage
+	$_.backcolor = ($tabsCol[$ti])
+	$_.Text = $temp[$ti]
+	$_.ToolTipText = $temp[$ti]
+$tabControl.Controls.Add($_)
+	$temp[$ti] = $_
+	$ti++
+}
+
+#tabPage System
+	$ti = 0
+	$tx = 10
+	$temp = @()
+
+	("MachineName","OSVersion","UserDomainName","UserName","Version").
+	ForEach{$_
+	write-host "Runde: tabPage System ... " $ti
+	write-host "-$_- " $_
+	$temp += "$_"
+	$label = $temp[$ti]
+	write-host "-$temp[$ti]- " $temp[$ti]
+	$_ = New-Object Windows.Forms.Label
+	$_.backcolor = "Khaki"
+	$_.Location = "$tx,30"
+	$_.Size = "130,20"
+	$_.Font = 'Consolas, 10pt, style=Bold'
+	$_.Text = $temp[$ti]
+	write-host "-$temp[$ti]- " $temp[$ti]
+$label.Controls.Add($_)
+	write-host "-$temp[$ti]- " $temp[$ti]
+	write-host "-$label- " $label
+	$ti++
+	$tx += 10
+	}
 
 $mainForm.ShowDialog()
