839d9677d0e1fb054b211a222eb10b33676be315
Fr3db3rt <fr3db3rt@gmail.com>
2019-04-05 14:04:55 +0200

    Create lastNewPTs.ps1

---
+++ lastNewPTs.ps1
@@ -0,0 +1,76 @@
+cls
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+write-host "=== #mainForm"
+	$mainForm = New-Object Windows.Forms.Form
+	$mainForm.StartPosition = "CenterScreen"
+	$mainForm.Size = "1024,768"
+	$mainForm.FormBorderStyle = "Fixed3D" #FixedSingle, FixedDialog, FixedToolWindow, None, Sizeable, SizableToolWindow, Fixed3D
+	$mainForm.backcolor = "Khaki"
+	$mainForm.Name = "mainForm"
+	$mainForm.Text = "LD System Wartungs Tool (ALPHA 2.0)"
+	#$mainForm.Icon="C:\Windows\System32\msconfig.exe"
+	#$mainForm.Topmost = $true
+
+write-host "=== #tabControl"
+	$tabControl = New-object Windows.Forms.TabControl
+	$tabControl.Alignment = "Top" #Bottom, Left, Right, Top
+	$tabControl.Appearance = "FlatButtons" #Buttons, FlatButtons, Normal
+	$tabControl.Padding = "10,5"
+	$tabControl.SizeMode = "Normal" #FillToRight, Fixed, Normal
+	$tabControl.Location = "75,85"
+	$tabControl.Size = "800,400"
+	$tabControl.ShowToolTips = "true"
+$mainForm.Controls.Add($tabControl)
+
+write-host "=== #tabPages"
+	$ti = 0
+	$tabsCol = @("Gainsboro","Khaki","Bisque")
+	$temp = @()
+	
+	("System","Veeam").
+	ForEach{$_
+	write-host "Runde: tabPages ... " $ti
+	write-host "-$_-: " $_
+	$temp += "$_"
+	write-host "-$temp- " $temp[$ti]
+	write-host ""
+	$_ = New-Object Windows.Forms.TabPage
+	$_.backcolor = ($tabsCol[$ti])
+	$_.Text = $temp[$ti]
+	$_.ToolTipText = $temp[$ti]
+$tabControl.Controls.Add($_)
+	$temp[$ti] = $_
+	$ti++
+}
+
+write-host "=== #tabPage System"
+	$ti = 0
+	$tx = 10
+	$temp = @()
+	$label = @()
+
+	("MachineName","OSVersion","UserDomainName","UserName","Version").
+	ForEach{$_
+	write-host "Runde: tabPage System ... " $ti
+	write-host "-$_- " $_
+	$temp += "$_"
+	$label = $temp[$ti]
+	write-host "-$temp[$ti]- " $temp[$ti]
+	$_ = New-Object Windows.Forms.Label
+	$_.backcolor = "Khaki"
+	$_.Location = "$tx,30"
+	$_.Size = "130,20"
+	$_.Font = 'Consolas, 10pt, style=Bold'
+	$_.Text = $temp[$ti]
+	write-host "-$temp[$ti]- " $temp[$ti]
+	write-host "-$label- vor: " $label
+$System.Controls.Add($_)
+	write-host "-$temp[$ti]- " $temp[$ti]
+	write-host "-$label- nach: " $label
+	$ti++
+	$tx += 10
+	}
+
+$mainForm.ShowDialog()
