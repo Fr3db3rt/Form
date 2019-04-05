1d7c5dda6b040636c59ddc9cce0a1a5b689b8d02
Fr3db3rt <fr3db3rt@gmail.com>
2019-04-05 14:04:17 +0200

    Create last-NewPTx.ps1

---
+++ last-NewPTx.ps1
@@ -0,0 +1,34 @@
+cls
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+write-host "=== #mainForm"
+	$mainForm = New-Object Windows.Forms.Form
+	$mainForm.StartPosition = "CenterScreen"
+	$mainForm.Size = "1024,768"
+	$mainForm.Name = "mainForm"
+	$mainForm.Text = "LD System Wartungs Tool (ALPHA 2.0)"
+
+write-host "=== #tabControl"
+	$tabControl = New-object Windows.Forms.TabControl
+	$tabControl.Location = "75,85"
+	$tabControl.Size = "800,400"
+$mainForm.Controls.Add($tabControl)
+
+write-host "=== #tabPages"
+	$System = New-Object Windows.Forms.TabPage
+	$System.Text = "System"
+$tabControl.Controls.Add($System)
+
+write-host "=== #tabPage System"
+	$tx = 10
+	$MachineName = New-Object Windows.Forms.Label
+	$MachineName.Location = "$tx,30"
+	$MachineName.Size = "130,20"
+	$MachineName.Text = "MachineName"
+$Systemx.Controls.Add($MachineName)
+	$tx += 10
+	$MachineName.Location = "$tx,30"
+$System.Controls.Add($MachineName)
+
+$mainForm.ShowDialog()
